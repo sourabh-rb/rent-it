2d3b6ac4f3c14c4fb2adeea7bec0caa5dd2ff3dc
sourabh-rb <sourabh.rajeev@gmail.com>
2019-10-02 23:01:31 -0400

    Client Manager Return, Cancel and Modify functionality

--- Rentit/src/main/java/com/rentit/controller/ClientsController.java
+++ Rentit/src/main/java/com/rentit/controller/ClientsController.java
@@ -1,16 +1,21 @@
 package com.rentit.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.rentit.model.Clients;
 import com.rentit.model.ClientsService;
-import com.rentit.test_class.TestClass;
 
 @Controller
 @RequestMapping("/")
@@ -22,10 +27,49 @@ public class ClientsController {
 	@RequestMapping("/clients")
 	public String listClient(Model model) {
 		
-		List<Clients> listClient = clientService.listAll();
-		
+		List<ArrayList<String>> listClient = clientService.listAll();
 		model.addAttribute("clients", listClient);
 		return "client-manager";
+	}
+	
+	
+	@RequestMapping("/return/{id}")
+	public String returnVehicle(@PathVariable(name = "id") Long id) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();
+		clientService.setReturnDate(id, dtf.format(now));
+	    return "redirect:/clients";       
+	}
+	
+	@RequestMapping("/cancel/{id}")
+	public String cancelBooking(@PathVariable(name = "id") Long id) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();
+		clientService.setCancelDate(id, dtf.format(now));
+	    return "redirect:/clients";       
+	}
+	
+	@RequestMapping("/modify/{id}")
+	public ModelAndView showEditProductPage(@PathVariable(name = "id") int id) {
+	    ModelAndView mav = new ModelAndView("client-modify");
+	    Clients clientDetails = clientService.getClientInfo(id);
+	    mav.addObject("modify", clientDetails);
+	     
+	    return mav;
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveEdit(@ModelAttribute("modify") Clients clientDetails) {
+		
+		clientService.saveClientInfo(clientDetails);
+	     
+	    return "redirect:/clients";
+	}
+	
+	@RequestMapping(value = "/cancel", method = RequestMethod.POST)
+	public String cancelEdit(@ModelAttribute("modify") Clients clientDetails) {
+		 
+	    return "redirect:/clients";
 	}
 	
 	
--- Rentit/src/main/java/com/rentit/model/Bookings.java
+++ Rentit/src/main/java/com/rentit/model/Bookings.java
@@ -1,72 +1,71 @@
-//package com.rentit.model;
-//
-//import javax.persistence.Entity;
-//
-//@Entity
-//public class Bookings {
-//	private Long bookingid;
-//	private String bookingTS;
-//	private String returnDate;
-//	private String startDate;
-//	private String dueDate;
-//	private String cancelDate;
-//	private Long clientid;
-//	
-//	protected Bookings() {}
-//	
-//	public Bookings(Long id, String bookingTS, String returnDate, String startDate, 
-//			String dueDate, String cancelDate, Long clientid) {
-//		this.bookingid = id;
-//		this.bookingTS = bookingTS;
-//		this.returnDate = returnDate;
-//		this.startDate = startDate;
-//		this.dueDate = dueDate;
-//		this.cancelDate = cancelDate;
-//		this.clientid = clientid;
-//	}
-//	public Long getBookingid() {
-//		return bookingid;
-//	}
-//	public void setBookingid(Long bookingid) {
-//		this.bookingid = bookingid;
-//	}
-//	public String getBookingTS() {
-//		return bookingTS;
-//	}
-//	public void setBookingTS(String bookingTS) {
-//		this.bookingTS = bookingTS;
-//	}
-//	public String getReturnDate() {
-//		return returnDate;
-//	}
-//	public void setReturnDate(String returnDate) {
-//		this.returnDate = returnDate;
-//	}
-//	public String getStartDate() {
-//		return startDate;
-//	}
-//	public void setStartDate(String startDate) {
-//		this.startDate = startDate;
-//	}
-//	public String getDueDate() {
-//		return dueDate;
-//	}
-//	public void setDueDate(String dueDate) {
-//		this.dueDate = dueDate;
-//	}
-//	public String getCancelDate() {
-//		return cancelDate;
-//	}
-//	public void setCancelDate(String cancelDate) {
-//		this.cancelDate = cancelDate;
-//	}
-//	public Long getClientid() {
-//		return clientid;
-//	}
-//	public void setClientid(Long clientid) {
-//		this.clientid = clientid;
-//	}
-//	
-//	
-//
-//}
+package com.rentit.model;
+
+
+
+public class Bookings {
+	private Long bookingid;
+	private String bookingTS;
+	private String returnDate;
+	private String startDate;
+	private String dueDate;
+	private String cancelDate;
+	private Long clientid;
+	
+	protected Bookings() {}
+	
+	public Bookings(Long id, String bookingTS, String returnDate, String startDate, 
+			String dueDate, String cancelDate, Long clientid) {
+		this.bookingid = id;
+		this.bookingTS = bookingTS;
+		this.returnDate = returnDate;
+		this.startDate = startDate;
+		this.dueDate = dueDate;
+		this.cancelDate = cancelDate;
+		this.clientid = clientid;
+	}
+	public Long getBookingid() {
+		return bookingid;
+	}
+	public void setBookingid(Long bookingid) {
+		this.bookingid = bookingid;
+	}
+	public String getBookingTS() {
+		return bookingTS;
+	}
+	public void setBookingTS(String bookingTS) {
+		this.bookingTS = bookingTS;
+	}
+	public String getReturnDate() {
+		return returnDate;
+	}
+	public void setReturnDate(String returnDate) {
+		this.returnDate = returnDate;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+	public String getCancelDate() {
+		return cancelDate;
+	}
+	public void setCancelDate(String cancelDate) {
+		this.cancelDate = cancelDate;
+	}
+	public Long getClientID() {
+		return clientid;
+	}
+	public void setClientID(Long clientid) {
+		this.clientid = clientid;
+	}
+	
+	
+
+}
