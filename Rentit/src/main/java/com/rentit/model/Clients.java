f6f26179f82d6491e699203bf99251c69187d183
Anusha KT <kt.anusha21@gmail.com>
2019-10-10 00:03:59 -0400

    Adding form validation and template

--- Rentit/src/main/java/com/rentit/model/Clients.java
+++ Rentit/src/main/java/com/rentit/model/Clients.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * CLients model class
@@ -13,11 +15,24 @@ import javax.persistence.Id;
 @Entity
 public class Clients {
 	private Long clientid;
+	
+	@NotNull
+	@Size(min=2, max=30)
 	private String first_name;
+	
+	@NotNull
+	@Size(min=2, max=30)
 	private String last_name;
+	
+	@NotNull
 	private String licence_number;
+	
+	@NotNull
 	private String licence_validity;
+	
+	@NotNull
 	private String phone;
+	
 	private Long clerkid;
 	
 	
