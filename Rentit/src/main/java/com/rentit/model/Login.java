bff5d2eac5f56c9cc6006f3a0f4f1ff6f54c7b4b
saicharanduduka <charan140494@gmail.com>
2019-10-14 20:16:19 -0400

    Registration admin link

--- Rentit/src/main/java/com/rentit/model/Login.java
+++ Rentit/src/main/java/com/rentit/model/Login.java
@@ -28,23 +28,6 @@ public class Login {
    this.password=Password;
  }
  
- public boolean addCredentials(String username, String password) {
-	 if(credHashMap.containsKey(username)) {
-		 return false;
-	 }else {
-	 credHashMap.put(username, password);
-	 return true;
-	 }
-	}
-	
-public boolean checkPassword(String username, String password) {
-		if(credHashMap.containsKey(username)) {
-			if(password.equals(credHashMap.get(username))) 
-				return true;
-			return false;
-		}
-		return false;
-		
-	}
+ 
 
 }
