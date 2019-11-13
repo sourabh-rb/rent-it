f81cd5e4beeccd5ad0eab438f28207fafb582235
sourabh-rb <sourabh.rajeev@gmail.com>
2019-11-12 23:00:10 -0500

    Interfaced Transactions page viewing and filtering #127

---
+++ Rentit/src/main/java/com/rentit/model/BookingFilter.java
@@ -0,0 +1,73 @@
+package com.rentit.model;
+
+public class BookingFilter {
+  private String clientFirstName;
+  private String clientLastName;
+  private String vehicleMake;
+  private String vehicleModel;
+  private String dueDate;
+  private String startDate;
+  
+  /**
+   * @return the clientFirstName
+   */
+  public String getClientFirstName() {
+    return clientFirstName;
+  }
+  /**
+   * @param clientFirstName the clientFirstName to set
+   */
+  public void setClientFirstName(String clientFirstName) {
+    this.clientFirstName = clientFirstName;
+  }
+  /**
+   * @return the vehicleMake
+   */
+  public String getVehicleMake() {
+    return vehicleMake;
+  }
+  /**
+   * @param vehicleMake the vehicleMake to set
+   */
+  public void setVehicleMake(String vehicleMake) {
+    this.vehicleMake = vehicleMake;
+  }
+  /**
+   * @return the dueDate
+   */
+  public String getDueDate() {
+    return dueDate;
+  }
+  /**
+   * @param dueDate the dueDate to set
+   */
+  public void setDueDate(String dueDate) {
+    this.dueDate = dueDate;
+  }
+  /**
+   * @return the startDate
+   */
+  public String getStartDate() {
+    return startDate;
+  }
+  /**
+   * @param startDate the startDate to set
+   */
+  public void setStartDate(String startDate) {
+    this.startDate = startDate;
+  }
+  public String getClientLastName() {
+    return clientLastName;
+  }
+  public void setClientLastName(String clientLastName) {
+    this.clientLastName = clientLastName;
+  }
+  public String getVehicleModel() {
+    return vehicleModel;
+  }
+  public void setVehicleModel(String vehicleModel) {
+    this.vehicleModel = vehicleModel;
+  }
+
+}
+
